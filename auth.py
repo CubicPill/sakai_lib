aabf1fa26a4921575c904408aea420606a9223ce
Wilton Norris <huazhengchang97@gmail.com>
2017-10-22 23:25:10 +0800

    complete sites fetching

--- auth.py
+++ auth.py
@@ -1,15 +1,48 @@
 import requests
 from sakai import Sakai
+from bs4 import BeautifulSoup
+from exceptions import SakaiException
 
-LOGIN_URL = ''
+LOGIN_URL = 'http://sakai.sustc.edu.cn/portal/pda/?force.login=yes'
 
 
 def cas_login(sid, password):
     session = requests.Session()
-    return session.cookies
+    session.headers = {
+        'Host': 'sakai.sustc.edu.cn',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) '
+                      'AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'DNT': '1',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
+    }
+    resp = session.get(LOGIN_URL)
+    soup_login = BeautifulSoup(resp.content, 'html5lib')
+    info = {}
+    for element in soup_login.find('form', {'id': 'fm1'}).find_all('input'):
+        if element.has_attr('name'):
+            value = ''
+            if element.has_attr('value'):
+                value = element['value']
+            info[element['name']] = value
+
+    info['username'] = sid
+    info['password'] = password
+
+    r = session.post(resp.url, data=info, timeout=30)
+    soup_response = BeautifulSoup(r.content, 'html5lib')
+    err = soup_response.find('div', {'class': 'errors', 'id': 'msg'})
+    if err:
+        raise SakaiException('Login failed')
+
+    return session.cookies.get('JSESSIONID',path='/')
 
 
 class SakaiAuth(Sakai):
     def __init__(self, sid, password):
-        cookies = cas_login(sid, password)
-        Sakai.__init__(self, cookies)
+        jsessionid = cas_login(sid, password)
+        Sakai.__init__(self, jsessionid)
--- sakai.py
+++ sakai.py
@@ -1,12 +1,11 @@
 import requests
-
+from requests.cookies import cookiejar_from_dict
 from exceptions import SakaiException
+from bs4 import BeautifulSoup
 
 
 class Sakai:
-    def __init__(self, cookies: str):
-        if 'JSESSIONID' not in cookies:
-            raise SakaiException('No JSESSIONID found in cookies')
+    def __init__(self, jsessionid):
         self.session = requests.session()
         self.session.headers = {
             'Host': 'sakai.sustc.edu.cn',
@@ -18,12 +17,21 @@ class Sakai:
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'DNT': '1',
             'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
+            'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4',
         }
-        self.session.cookies = cookies
+        self.session.cookies.set(name='JSESSIONID', value=jsessionid)
 
     def sites_joined(self):
-        self.session.get('')
+        mp = self.session.get('http://sakai.sustc.edu.cn/portal')
+        with open('t.html', 'wb') as f:
+            f.write(mp.content)
+        soup = BeautifulSoup(mp.content, 'html5lib')
+        sites = dict()
+
+        for li in soup.find('ul', {'id': 'pda-portlet-site-menu'}).find_all('li')[1:]:
+            sites[li.span.a['href'].split('/')[-1]] = li.span.a.text.strip()
+
+        return sites
 
     def assignments(self, site_id):
         pass
