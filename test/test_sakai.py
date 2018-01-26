126b2054278e649b34e80b45fdea81e97b09cfdf
CubicPill <huazhengchang97@gmail.com>
2018-01-26 23:08:22 +0800

    add unittest

--- test/test_sakai.py
+++ test/test_sakai.py
@@ -1,23 +1,31 @@
 from unittest import TestCase
 
-from sakailib import SakaiAuth
+from sakailib import SakaiAuth, Sakai
+from sakailib.exceptions import NotLoggedIn
 from .test_config import config
 
 sakai = SakaiAuth(config['usn'], config['pwd'])
+sakai_nologin = Sakai('')
 
 
 class TestSakai(TestCase):
     def test_fetch(self):
-        self.fail()
+        sakai.fetch('https://sakai.sustc.edu.cn')
+
+    def test_fetch_no_login(self):
+        try:
+            sakai_nologin.fetch('https://sakai.sustc.edu.cn')
+        except Exception as e:
+            self.assertEqual(type(e), NotLoggedIn)
 
     def test_logout(self):
         self.fail()
 
     def test_sites_joined(self):
-        self.fail()
+        sakai.sites_joined()
 
     def test_get_sites_list(self):
-        self.fail()
+        sakai.get_sites_list()
 
     def test_site_tools_list(self):
         self.fail()
