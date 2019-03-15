66e6febec94a29d3df6fa32448bdd6d12f63d46e
lightsing <light.tsing@gmail.com>
2019-03-15 14:26:42 +0800

    add dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='sakailib',
     version='v1.0.3',
     packages=['sakailib'],
+    install_requires=[
+        'bs4',
+        'html5lib'
+    ],
     url='https://github.com/cubicpill/sakai_lib',
     license='GPL v3.0',
     author='CubicPill',
